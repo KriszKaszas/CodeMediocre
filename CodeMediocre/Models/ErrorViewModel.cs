e821d46cafde74733d894e211fd6c3a1b1e8aff1
Krisz <kriszrokakaszas@gmail.com>
2019-06-27 22:14:50 +0200

    [PROJECT RESTARTED] Initial commit - Web app project created

---
+++ CodeMediocre/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeMediocre.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
